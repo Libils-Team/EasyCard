c1c01a987f0d5b4943e08f0fb5f03573d34d0f03
Ivan <45362427+Efcolipt@users.noreply.github.com>
2021-10-23 17:27:53 +0300

    configure app

---
+++ client-app/src/store/modules/app.js
@@ -0,0 +1,33 @@
+const state = () => ({
+  locale: "ru",
+});
+
+const mutations = {
+  SET_LOCALE(state, val) {
+    state.locale = val;
+  },
+};
+
+const actions = {
+  changeLocale({ commit }, val) {
+    commit("SET_LOCALE", val);
+  },
+  init({ dispatch }) {
+    const lang = localStorage.getItem("locale") || window.navigator.language;
+    dispatch("changeLocale", lang);
+  },
+};
+
+const getters = {
+  getLocale({ locale }) {
+    return locale;
+  },
+};
+
+export default {
+  namespaced: true,
+  state,
+  mutations,
+  actions,
+  getters,
+};
