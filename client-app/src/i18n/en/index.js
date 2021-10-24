8802000fc0e301dc13b3645e1a09e8092f520eaf
Ivan <45362427+Efcolipt@users.noreply.github.com>
2021-10-25 01:32:08 +0300

    add shop store

--- client-app/src/i18n/en/index.js
+++ client-app/src/i18n/en/index.js
@@ -18,7 +18,7 @@ export default {
           },
           {
             type: "link",
-            label: "About company",
+            label: "About Us",
             path: paths.aboutUs,
           },
           {
--- client-app/src/store/index.js
+++ client-app/src/store/index.js
@@ -1,9 +1,11 @@
 import { createStore } from "vuex";
 import app from "@/store/modules/app";
+import shop from "@/store/modules/shop";
 
 export default createStore({
   modules: {
     app,
+    shop,
   },
   namespaced: true,
 });
