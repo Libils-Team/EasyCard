0aac64438460bf68e359b7ba44c04dbbbc4a6acb
Ivan <45362427+Efcolipt@users.noreply.github.com>
2021-10-24 19:04:44 +0300

    rework menu item

--- client-app/src/components/MenuItem.vue
+++ client-app/src/components/MenuItem.vue
@@ -1,19 +1,29 @@
 <template>
   <router-link
+    class="menu-item__link flex align-center"
     v-if="item.type === 'link' || item.type === 'call-modal'"
     :to="item.path"
-    class="flex align-center"
   >
     <i v-if="item.icon" :class="[item.icon, { 'mr-2': item.label }]"></i>
     {{ item.label }}
   </router-link>
-  <p v-if="item.type === 'string'" class="flex align-center">
+  <p v-if="item.type === 'string'" class="menu-item__text flex align-center">
     <i v-if="item.icon" :class="[item.icon, { 'mr-2': item.label }]"></i>
     {{ item.label }}
   </p>
-  <ul class="dropdown-menu" v-if="item.type === 'dropdown'">
+  <div v-if="item.type === 'lang'" class="menu-item__lang flex align-center">
+    <p
+      class="mr-2"
+      v-for="lang in item.languages"
+      :key="JSON.stringify(lang)"
+      @click="$store.dispatch('app/changeLocale', lang.val)"
+    >
+      {{ lang.label }}
+    </p>
+  </div>
+  <ul class="menu-item__dropdown dropdown-menu" v-if="item.type === 'dropdown'">
     <li
-      class="dropdown-item"
+      class="dropdown-menu__item"
       v-for="dropdownItem in item.dropdownMenu"
       :key="JSON.stringify(dropdownItem)"
     >
