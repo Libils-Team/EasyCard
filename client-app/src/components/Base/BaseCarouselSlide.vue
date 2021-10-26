4698f3a185b9795ae8d48e812e7795dee722c7e5
Ivan <45362427+Efcolipt@users.noreply.github.com>
2021-10-26 21:51:54 +0300

    add carousel

---
+++ client-app/src/components/Base/BaseCarouselSlide.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="carousel-slide">
+    <slot></slot>
+  </div>
+</template>
+
+<style scoped>
+    .carousel-slide {
+    height: 100%;
+    min-height: 1px;
+    display: block;
+    }
+</style>
