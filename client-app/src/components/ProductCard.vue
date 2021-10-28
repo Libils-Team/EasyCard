39dade88dd3118729e402aa3bbbeddc273a005ff
Ivan <45362427+Efcolipt@users.noreply.github.com>
2021-10-28 21:46:13 +0300

    add pages

--- client-app/src/components/ProductCard.vue
+++ client-app/src/components/ProductCard.vue
@@ -1,10 +1,50 @@
 <template>
-  <div class="card-product"></div>
+  <div class="card-product">
+    {{ id }}
+  </div>
 </template>
 
 <script>
 export default {
   name: "ProductCard",
+  props: {
+    priceCurrent: {
+      type: [Number, String],
+      required: true,
+    },
+    priceOld: {
+      type: [Number, String],
+      default: "",
+    },
+    description: {
+      type: String,
+      default: "",
+    },
+    id: {
+      type: [String, Number],
+      required: true,
+    },
+    title: {
+      type: String,
+      required: true,
+    },
+    imagePath: {
+      type: String,
+      required: true,
+    },
+    productCategory: {
+      type: Object,
+      default: () => ({}),
+    },
+    productCode: {
+      type: [String, Number],
+      required: true,
+    },
+    productData: {
+      type: [String, Object],
+      default: "",
+    },
+  },
 };
 </script>
 
