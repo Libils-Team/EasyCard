c539ed2109324ee8b122f5138e6e02094051ae0a
Ivan <45362427+Efcolipt@users.noreply.github.com>
2021-11-01 06:44:52 +0300

    refactoring scss

---
+++ client-app/src/components/Product/ProductCardCart.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="card-product">
+    {{ product.id }}
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ProductCardCart",
+  props: {
+    product: {
+      type: Object,
+      default: () => ({}),
+    },
+  },
+};
+</script>
