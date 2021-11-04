4dc4991bd4fbcf3f4d16d97f01919a2834ec19bf
Ivan <45362427+Efcolipt@users.noreply.github.com>
2021-11-04 04:38:43 +0300

    Fix breadcrumbs

--- client-app/src/components/Breadcrumbs.vue
+++ client-app/src/components/Breadcrumbs.vue
@@ -1,17 +1,20 @@
 <template>
-  <div class="breadcrumbs">
+  <div class="breadcrumbs flex" v-if="breadcrumbs.length">
     <div class="breadcrumbs-item">
-      <router-link to="/">
+      <router-link class="breadcrumbs-item__link text-half-gray mr-3" to="/">
         <span>Главная</span>
       </router-link>
     </div>
     <div
-      class="breadcrumbs-item"
+      class="breadcrumbs-item mr-3"
       v-for="({ title, path }, i) in breadcrumbs"
       :key="'breadcrumb_item_' + i"
     >
-      /
-      <router-link :to="path">
+      <span class="text-half-gray">/</span>
+      <router-link
+        class="breadcrumbs-item__link text-half-gray ml-3"
+        :to="path"
+      >
         <span> {{ title }}</span>
       </router-link>
     </div>
